7f7c247edb436625ff70c170e7dffdb5fad91759
Dmitry Sharypov <Sharypov_D@mail.ru>
2023-06-24 00:00:08 +0300

    Cli ask name code added

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,2 +1,17 @@
-package hexlet.code;public class Cli {
+package hexlet.code;
+import  java.util.Scanner;
+public class Cli {
+    public static String enterYouName(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String userName = "";
+       if (scanner.hasNextLine()) {
+            userName = scanner.next();
+            System.out.println("Hello, " + userName + "!");
+        } else {
+            System.out.println("Sorry, You name is not a string!");
+        }
+        scanner.close();
+        return userName;
+    }
 }
